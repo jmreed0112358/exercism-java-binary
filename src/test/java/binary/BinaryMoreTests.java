36ae367196f5af119c94520a84d83b5a41cfeef7
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 12:27:18 -0600

    Unit tests.

---
+++ src/test/java/binary/BinaryMoreTests.java
@@ -0,0 +1,13 @@
+package binary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryMoreTests
+{	
+	@Test
+	public void test_constructor_InvalidInputs_NullString_ThrowsException( ) {
+		fail( "Not yet implemented" );
+	}
+}
