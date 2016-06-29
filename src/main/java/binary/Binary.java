dbb644f4d842d9ce1c294bb7d312a4e8f51f476c
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 12:30:00 -0600

    Change sanitize(...) to validateInput(...)
    Add some comments.

--- src/main/java/binary/Binary.java
+++ src/main/java/binary/Binary.java
@@ -12,7 +12,14 @@ public class Binary
 		throw new NotImplementedException();
 	}
 	
-	private String sanitize( String rawInput ) {
+	/**
+	 * Validates input.
+	 * Input is valid if every Character in the string is a '0' or '1'.
+	 * Return empty string if the string is invalid.
+	 * @param rawInput
+	 * @return
+	 */
+	private String validateInput( String rawInput ) {
 		throw new NotImplementedException();
 	}
 }
